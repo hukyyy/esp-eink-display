0b15474e9290df2def3bf8be3dc818d65c7aa279
Elliot Huet <huky2005@gmail.com>
2025-08-04 12:37:44 +0200

    add joke api request and serde

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,114 @@
 use esp_idf_svc::eventloop::EspSystemEventLoop;
+use esp_idf_svc::hal::gpio::Level;
 use esp_idf_svc::hal::{
-    gpio::{Gpio2, Output, PinDriver},
+    gpio::{Gpio2, PinDriver},
     peripherals::Peripherals,
 };
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 
+use log::info;
+
 use std::thread;
 use std::time::Duration;
 
-use std::sync::mpsc;
+use std::sync::mpsc::{self, Sender};
+
+use serde::{Deserialize, Serialize};
 
 mod wifi;
 
-fn main() {
+enum LedProgram {
+    Stable(Level),
+    Blink(Duration),
+    Shutdown,
+}
+
+struct InternalLed {
+    thread_handle: Option<thread::JoinHandle<()>>,
+    tx: Sender<LedProgram>,
+}
+
+impl<'a> InternalLed {
+    fn new(gpio2: Gpio2) -> InternalLed {
+        let (tx, rx) = mpsc::channel();
+        let mut pin = PinDriver::output(gpio2).unwrap();
+        let mut program = LedProgram::Stable(Level::High);
+
+        let thread_handle = thread::spawn(move || loop {
+            if let Ok(p) = rx.recv_timeout(Duration::from_millis(10)) {
+                program = p;
+            };
+
+            match program {
+                LedProgram::Stable(level) => {
+                    pin.set_level(level).unwrap();
+                    thread::sleep(Duration::from_millis(100));
+                }
+                LedProgram::Blink(duration) => {
+                    pin.set_low().unwrap();
+                    thread::sleep(duration);
+                    pin.set_high().unwrap();
+                    thread::sleep(duration - Duration::from_millis(10));
+                }
+                LedProgram::Shutdown => break,
+            }
+        });
+
+        InternalLed {
+            thread_handle: Some(thread_handle),
+            tx,
+        }
+    }
+
+    fn set_program(&mut self, program: LedProgram) {
+        self.tx.send(program);
+    }
+}
+
+impl Drop for InternalLed {
+    fn drop(&mut self) {
+        self.tx.send(LedProgram::Shutdown);
+        if let Some(handle) = self.thread_handle.take() {
+            let _ = handle.join();
+        }
+    }
+}
+
+fn main() -> ! {
     initialise_esp32();
 
     let peripherals = Peripherals::take().unwrap();
     let sys_loop = EspSystemEventLoop::take().unwrap();
     let nvs = EspDefaultNvsPartition::take().unwrap();
 
-    // let internal_led = InternalLed::new(peripherals.pins.gpio2);
-    let mut internal_led = PinDriver::output(peripherals.pins.gpio2).unwrap();
+    let mut internal_led = InternalLed::new(peripherals.pins.gpio2);
 
-    internal_led.set_high().unwrap();
+    internal_led.set_program(LedProgram::Blink(Duration::from_millis(500)));
 
-    let wifi_connection =
+    let mut wifi_connection =
         wifi::WifiConnection::new(peripherals.modem, sys_loop.clone(), nvs.clone());
 
+    internal_led.set_program(LedProgram::Stable(Level::High));
+
     loop {
-        internal_led.set_low().unwrap();
-        thread::sleep(Duration::from_millis(500));
-        internal_led.set_high().unwrap();
-        thread::sleep(Duration::from_millis(500));
+        thread::sleep(Duration::from_millis(5000));
+        info!("Getting joke!!!");
+
+        if let Ok(joke_json) =
+            wifi_connection.get_request("https://v2.jokeapi.dev/joke/Programming?type=single")
+        {
+            let joke: JokeResponse = serde_json::from_str(&joke_json).unwrap();
+
+            match joke.error {
+                false => {
+                    info!("Got a joke!");
+                    info!("{}", joke.joke);
+                }
+                true => {
+                    info!("Failed to get a joke");
+                }
+            }
+        }
     }
 }
 
@@ -42,4 +119,10 @@ fn initialise_esp32() {
 
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
+}
+
+#[derive(Deserialize, Serialize)]
+struct JokeResponse {
+    error: bool,
+    joke: String,
 }
